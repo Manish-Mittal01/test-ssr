f2756506902da3ba5dafba34f33a9e862307cf5a
Manish Mittal <mittalmanish2000@gmil.com>
2025-01-26 18:43:11 +0530

    fixed favicon and served html from public folder instead build folder

--- server/server.js
+++ server/server.js
@@ -11,25 +11,74 @@ import { StaticRouter } from "react-router-dom";
 const port = 8000;
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, "..", "build"), { index: false }));
+// app.use(express.static(path.resolve(__dirname, "..", "build"), { index: false }));
 
-app.get("*", (req, res) => {
-  const indexFile = path.resolve(__dirname, "..", "build", "index.html");
+app.get("/favicon.ico", (req, res) => {
+  const indexFile = path.resolve(__dirname, "..", "public", "favicon.ico");
+  //   const indexFile = path.resolve(__dirname, "..", "build", "index.html");
 
   fs.readFile(indexFile, "utf-8", (error, data) => {
     if (error) {
+      console.log("error", error);
       return res.status(500).send("Internal server error");
     }
 
+    res.sendFile(indexFile);
+
     // Use StaticRouter to pass the current URL for SSR
-    const context = {}; // Context to track routing
-    const renderedApp = ReactDOMServer.renderToString(
-      <StaticRouter location={req.url} context={context}>
+    // const context = {}; // Context to track routing
+    // const renderedApp = ReactDOMServer.renderToString(
+    //   <StaticRouter location={req.url} context={context}>
+    //     <App />
+    //   </StaticRouter>
+    // );
+
+    // return res.send(data.replace(`<div id="root"></div>`, `<div id="root">${renderedApp}</div>`));
+  });
+});
+
+app.get("*", (req, res) => {
+  const indexFile = path.resolve(__dirname, "..", "public", "index.html");
+  //   const indexFile = path.resolve(__dirname, "..", "build", "index.html");
+
+  fs.readFile(indexFile, "utf-8", (error, data) => {
+    if (error) {
+      console.log("error", error);
+      return res.status(500).send("Internal server error");
+    }
+
+    const appHtml = ReactDOMServer.renderToString(
+      <StaticRouter location={req.url} context={{}}>
         <App />
       </StaticRouter>
     );
+    const html = `
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>React SSR</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="/static/css/main.css" />
+      </head>
+      <body>
+        <div id="root">${appHtml}</div>
+        <script src="/static/js/main.js"></script>
+      </body>
+      </html>
+    `;
+    res.send(html);
 
-    return res.send(data.replace(`<div id="root"></div>`, `<div id="root">${renderedApp}</div>`));
+    // Use StaticRouter to pass the current URL for SSR
+    // const context = {}; // Context to track routing
+    // const renderedApp = ReactDOMServer.renderToString(
+    //   <StaticRouter location={req.url} context={context}>
+    //     <App />
+    //   </StaticRouter>
+    // );
+
+    // return res.send(data.replace(`<div id="root"></div>`, `<div id="root">${renderedApp}</div>`));
   });
 });
 
